27330326ee63ec2384a99486138e42a5976709c4
Masud Rana <masudrana58855@gmail.com>
2023-10-06 09:58:34 +0600

    add navbar and change some text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className="navbar bg-base-100">
   <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <Link to="/" className="btn btn-ghost normal-case text-xl">Book Motion</Link>
   </div>
   <div className="flex-none">
     <div className="dropdown dropdown-end">
