1e81be254ea0f2f5933b220c61b034c6bbe59c9c
Masud Rana <masudrana58855@gmail.com>
2023-10-07 21:40:30 +0600

    add login and signup button in navbar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Login from "../pages/auth/Login";
 import SignUp from "../pages/auth/SignUp";
+import Home from "../components/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout/>,
         children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
             {
                 path: '/login',
                 element: <Login/>
